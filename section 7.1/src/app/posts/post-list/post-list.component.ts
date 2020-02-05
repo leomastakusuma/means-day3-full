78e2df19ac77a15e21fae249cd9826aa12664aed
Leo Masta <leomastakusuma@gmail.com>
2020-02-05 09:25:40 +0700

    init

---
+++ section 7.1/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+import { PageEvent } from "@angular/material";
+
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  posts: Post[] = [];
+  isLoading = false;
+  totalPosts = 10;
+  postPerPage = 2;
+  pageSizeOptions=[1,2,3,5];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+      });
+  }
+
+  onChangePage(pageData : PageEvent){
+    console.log(pageData)
+  }
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
---
+++ section 7.2/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+import { PageEvent } from "@angular/material";
+
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  posts: Post[] = [];
+  isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions=[1,2,3,5];
+  currentPage =1;
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((post:Post[]) => {
+        this.isLoading = false;
+        this.posts = post;
+      });
+  }
+
+
+  onChangePage(pageData : PageEvent){
+    console.log(pageData)
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
